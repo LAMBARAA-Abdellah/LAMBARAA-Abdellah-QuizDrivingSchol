67b6d6443e774edb98c440fcfd3a2acc71faff95
Abdellah Lambaraa <abdellah17lambaraa@gmail.com>
2024-05-20 12:05:54 +0100

    organise jscode

--- main.js
+++ main.js
@@ -1,37 +1,42 @@
 const quizData = [
-  {
-    question: "Which language runs in a web browser?",
-    a: "Java",
-    b: "C",
-    c: "Python",
-    d: "JavaScript",
-    correct: "d",
-  },
-  {
-    question: "What does CSS stand for?",
-    a: "Central Style Sheets",
-    b: "Cascading Style Sheets",
-    c: "Cascading Simple Sheets",
-    d: "Cars SUVs Sailboats",
-    correct: "b",
-  },
-  {
-    question: "What does HTML stand for?",
-    a: "Hypertext Markup Language",
-    b: "Hypertext Markdown Language",
-    c: "Hyperloop Machine Language",
-    d: "Helicopter Terminals Motorboats Lamborghinis",
-    correct: "a",
-  },
-  {
-    question: "What year was JavaScript Launched",
-    a: "1996",
-    b: "1995",
-    c: "1994",
-    d: "None of the above",
-    correct: "b",
-  },
-];
+    {
+      question: "Which language runs in a web browser?",
+      a: "Java",
+      b: "C",
+      c: "Python",
+      d: "JavaScript",
+      correct: "d",
+      imageSrc: "images/image1.jpg",
+    },
+    {
+      question: "What does CSS stand for?",
+      a: "Central Style Sheets",
+      b: "Cascading Style Sheets",
+      c: "Cascading Simple Sheets",
+      d: "Cars SUVs Sailboats",
+      correct: "b",
+      imageSrc: "images/image2.jpg",
+    },
+    {
+      question: "What does HTML stand for?",
+      a: "Hypertext Markup Language",
+      b: "Hypertext Markdown Language",
+      c: "Hyperloop Machine Language",
+      d: "Helicopter Terminals Motorboats Lamborghinis",
+      imageSrc: "images/image3.jpg",
+      correct: "a",
+    },
+    {
+      question: "What year was JavaScript Launched",
+      a: "1996",
+      b: "1995",
+      c: "1994",
+      d: "None of the above",
+      correct: "b",
+      imageSrc: "images/image4.jpg",
+    },
+  ];
+
 
 const quiz = document.getElementById("quiz");
 const answerEls = document.querySelectorAll(".answer");
@@ -40,6 +45,7 @@ const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
+const imageEl = document.getElementById("quiz-image");
 const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
@@ -55,6 +61,7 @@ function loadQuiz() {
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
+    imageEl.src = currentQuizData.imageSrc;
 }
 
 function deselectAnswers() {
@@ -88,7 +95,7 @@ submitBtn.addEventListener('click', () => {
                 <h2>You Answered ${score} / ${quizData.length} questions correctly</h2>
 
                 <button onClick="location.reload()">Reload</button>
-                <a href="landing_page.html">Home</a>
+                <a href="index.html">Home</a>
             `
         }
     }
